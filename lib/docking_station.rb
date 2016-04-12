14d6b75052311597f8bd394dea27ca8b8dc611a9
Michael Harrison <harrim91@hotmail.co.uk>
2016-04-12 12:27:46 +0100

    Added dock and docked_bikes method + tests

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,19 @@
 require "./lib/bike.rb"
 
 class DockingStation
+  attr_accessor :docked_bikes
+
+  def initialize
+    @docked_bikes = []
+  end
+
   def release_bike
     Bike.new
   end
+
+  def dock(bike)
+    self.docked_bikes << bike
+  end
+
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,9 +2,17 @@ require "docking_station"
 
 describe DockingStation do
   it {is_expected.to respond_to(:release_bike)}
+  it {is_expected.to respond_to(:dock).with(1).argument}
+  it {is_expected.to respond_to(:docked_bikes)}
 
   it "releases a working bike" do
     bike = subject.release_bike
     expect(bike).to be_working
+  end
+
+  it "can dock a bike" do
+    bike = subject.release_bike
+    subject.dock(bike)
+    expect(subject.docked_bikes).to include bike
   end
 end
