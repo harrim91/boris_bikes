c9d01a4ef0a5d8963eebef7c0c0c4897be867c18
Wayne Rumble <rumblewayne@gmail.com>
2016-04-13 11:11:43 +0100

    working? method added, working? test added.

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -2,8 +2,9 @@ require "dockingstation"
 
 describe DockingStation do
   it { expect(subject).to respond_to :release_bike }
-  it "#release_bike returns a bike" do
+  it "#release_bike returns a working bike" do
     bike = subject.release_bike
     expect(bike).to be_a Bike
+    expect(bike.working?).to be true
   end
 end
