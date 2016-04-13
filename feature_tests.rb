c8fed552922d9f8f56b6227d6bd32688da92ea51
Michael Harrison <harrim91@hotmail.co.uk>
2016-04-13 12:14:34 +0100

    #dock returns docked bike, added responds to #bike test, refactored feature tests

--- feature_tests.rb
+++ feature_tests.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 require "./lib/dockingstation"
 require "./lib/bike"
 
@@ -7,25 +9,24 @@ stn = DockingStation.new
 # So that I can use a bike,
 # I'd like a docking station to release a bike.
 
-bike = stn.release_bike  #=> Bike object
-raise "bike is not a Bike" unless bike.class == Bike
+raise "bike is not a Bike" unless stn.release_bike.class == Bike
 
 # As a person,
 # So that I can use a good bike,
 # I'd like to see if a bike is working
 
-bike.working? #=> true
+raise "bike is not working" unless stn.release_bike.working?
 
 # As a member of the public
 # So I can return bikes I've hired
 # I want to dock my bike at the docking station
 
-bike2 = Bike.new
+bike = Bike.new
 
-raise "dock does not return docked bike" unless stn.dock(bike2) == bike2
+raise "#dock does not return docked bike" unless stn.dock(bike) == bike
 
 # As a member of the public
 # So I can decide whether to use the docking station
 # I want to see a bike that has been docked
 
-stn.bike        #=> bike2
+raise "#bike does not return the docked bike" unless stn.bike == bike
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -3,6 +3,7 @@ require "dockingstation"
 describe DockingStation do
   it { expect(subject).to respond_to :release_bike }
   it { expect(subject).to respond_to(:dock).with(1).argument }
+  it { expect(subject).to respond_to :bike }
   it "#release_bike returns a working bike" do
     bike = subject.release_bike
     expect(bike).to be_a Bike
